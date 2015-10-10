e221bd827594eecff361e0373749f56cb72ef27f
jdalton308 <jdalton308@yahoo.com>
2015-10-10 16:03:49 -0700

    New gulpfile.js, and re-organized a little

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,23 @@
+
 var gulp = require('gulp');
 var less = require('gulp-less');
- 
+var concat = require('gulp-concat');
+
+var styleFiles = [
+        'css/less/main.less'
+    ];
+
+
 
 gulp.task('styles', function() {
-    gulp.src(['./src/less/main.less'])
+    gulp.src(styleFiles)
+        .pipe(concat('main.css'))
         .pipe(less())
-        // .pipe(minifyCSS())
-        .pipe(gulp.dest('style.css'))
+        .pipe(gulp.dest('css/'));
 });
 
-gulp.task('default', function() {
-    // gulp.run('scripts', 'styles', 'html');
-    gulp.run('styles');
-
-    // gulp.watch('app/src/**', function(event) {
-    //     gulp.run('scripts');
-    // });
-
-    gulp.watch('app/css/**', function(event) {
-        gulp.run('styles');
-    });
-
-    // gulp.watch('app/**/*.html', function(event) {
-    //     gulp.run('html');
-    // });
+gulp.task('watch', function(){
+    gulp.watch(['css/less/*.less'], ['styles']);
 });
+
+gulp.task('default', ['styles', 'watch']);
