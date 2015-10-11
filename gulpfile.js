240670d3b34ed0173bf49f6a60f69dcb5a130d43
jdalton308 <jdalton308@yahoo.com>
2015-10-10 19:49:23 -0700

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var less = require('gulp-less');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 var styleFiles = [
         'css/less/main.less'
     ];
+var jsFiles = [
+		'bower_components/jquery/dist/jquery.min.js',
+		'js/src/main.js'
+	];
 
 
 
@@ -16,8 +22,17 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('css/'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch(['css/less/*.less'], ['styles']);
+gulp.task('scripts', function(){
+	gulp.src(jsFiles)
+		.pipe(concat('app.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('js/'))
+		.on('error', gutil.log);
 });
 
-gulp.task('default', ['styles', 'watch']);
+gulp.task('watch', function(){
+    gulp.watch(['css/less/*.less'], ['styles']);
+    gulp.watch(['js/src/*.js'], ['scripts']);
+});
+
+gulp.task('default', ['styles', 'scripts', 'watch']);
