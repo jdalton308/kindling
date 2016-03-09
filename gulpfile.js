dfb4033690ee2e3ae9d62f8126ddc9081dce7788
jdalton308 <jdalton308@yahoo.com>
2016-03-08 18:34:25 -0800

    Reorg for src/ and build/ directories

--- gulpfile.js
+++ gulpfile.js
@@ -4,35 +4,58 @@ var gutil = require('gulp-util');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var htmlMin = require('gulp-htmlmin');
+var server = require('gulp-server-livereload');
 
 var styleFiles = [
-        'css/scss/main.scss'
+        './src/scss/main.scss'
     ];
 var jsFiles = [
 		'node_modules/jquery/dist/jquery.min.js',
-		'js/src/main.js'
+		'./src/js/*/**.js'
 	];
-
+var htmlFiles = [
+		'./src/html/*/**.html'
+]
 
 
 gulp.task('styles', function() {
     gulp.src(styleFiles)
         .pipe(concat('main.css'))
         .pipe(less())
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('./build/css/'));
 });
 
 gulp.task('scripts', function(){
 	gulp.src(jsFiles)
 		.pipe(concat('app.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('js/'))
+		.pipe(gulp.dest('./build/js/'))
 		.on('error', gutil.log);
 });
 
-gulp.task('watch', function(){
-    gulp.watch(['css/scss/*.scss'], ['styles']);
-    gulp.watch(['js/src/*.js'], ['scripts']);
+gulp.task('html', function(){
+	gulp.src(htmlFiles)
+		.pipe(htmlMin({
+			collapseWhitespace: true
+		}))
+		.pipe(gulp.dest('./build/'))
 });
 
-gulp.task('default', ['styles', 'scripts', 'watch']);
+gulp.task('server', function(){
+	gulp.src('./build')
+		.pipe(server({
+			livereload: true,
+			open: true
+		}));
+});
+
+gulp.task('watch', function(){
+    gulp.watch(['./src/css/scss/*/**.scss'], ['styles']);
+    gulp.watch(['./src/js/*/**.js'], ['scripts']);
+    gulp.watch(['./src/html/*/**.html'], ['html'])
+});
+
+gulp.task('build', ['styles', 'scripts', 'html']);
+
+gulp.task('default', ['build', 'watch', 'server']);
